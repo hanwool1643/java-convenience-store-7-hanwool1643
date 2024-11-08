8668944111f3af86f1f249b9f9cf19dfd756d1ba
hanwool <hanwe92@gmail.com>
2024-11-08 10:50:02 +0900

    feat(InputParser): Add inventoryParser method
    To print inventory detail as same as format provided,
    added parser methods

---
+++ src/main/java/store/view/InputParser.java
@@ -0,0 +1,64 @@
+package store.view;
+
+import java.util.Objects;
+import store.common.constants.StringConstants;
+
+public class InputParser {
+    public static String inventoryParser(String name, Long price, Long quantity, String promotion) {
+        StringBuilder stringBuilderWithDash = new StringBuilder(giveBlankToEnd(StringConstants.DASH));
+
+        return appendAll(
+                stringBuilderWithDash,
+                giveBlankToEnd(name),
+                giveBlankToEnd(parsePrice(price)),
+                giveBlankToEnd(parseQuantity(quantity)),
+                parsePromotion(promotion)
+        ).toString();
+    }
+
+    private static String giveBlankToEnd(String name) {
+        return name + StringConstants.BLANK;
+    }
+
+    private static String parsePromotion(String promotion) {
+        String promotionNullConverted = promotion;
+        if (Objects.equals(promotion, "null")) {
+            promotionNullConverted = "";
+        }
+        return promotionNullConverted;
+    }
+
+    private static String parseQuantity(Long quantity) {
+        String quantityWithPieces = quantity.toString() + StringConstants.PIECES;
+        if (quantity == 0) {
+            quantityWithPieces = StringConstants.OUT_OF_STOCK;
+        }
+        return quantityWithPieces;
+    }
+
+    private static String parsePrice(Long price) {
+        if (price >= 1000) {
+            String[] splitPrice = price.toString().split("");
+            return giveCommaToPrice(splitPrice).toString();
+        }
+        return price+ StringConstants.WON;
+    }
+
+    private static StringBuilder giveCommaToPrice(String[] splitPrice) {
+        StringBuilder priceWithCurrency = new StringBuilder();
+        for (int i = 0; i < splitPrice.length; i++) {
+            if (i % 3 == 1) {
+                priceWithCurrency.append(StringConstants.COMMA);
+            }
+            priceWithCurrency.append(splitPrice[i]);
+        }
+        return priceWithCurrency.append(StringConstants.WON);
+    }
+
+    private static StringBuilder appendAll(StringBuilder stringbuilder, String... strings) {
+        for (String string : strings) {
+            stringbuilder.append(string);
+        }
+        return stringbuilder;
+    }
+}
