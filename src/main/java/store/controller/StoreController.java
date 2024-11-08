22e29bb297fd8704ad6e93958c969dc715a9e7aa
hanwool <hanwe92@gmail.com>
2024-11-08 13:13:29 +0900

    fix(StoreController): Apply service method changes

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,11 +1,9 @@
 package store.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import store.common.FileReader;
 import store.common.constants.AddressConstant;
-import store.common.constants.StringConstants;
 import store.domain.dto.ProductDto;
 import store.service.ProductService;
 import store.view.OutputView;
@@ -17,25 +15,13 @@ public class StoreController {
 
     public void open() {
         OutputView.printWelcome();
+        // 제품
         Scanner productsFile = FileReader.readFile(AddressConstant.productFilePath);
-        List<ProductDto> inventory = new ArrayList<>();
         ProductService productService = new ProductService();
-        int scannerLine = 0;
-        while (productsFile.hasNextLine()) {
-            String line = productsFile.nextLine();
-            if (scannerLine != 0) {
-                String[] productsInfo = line.split(StringConstants.COMMA);
-                ProductDto product = productService.createProduct(
-                        productsInfo[0],
-                        Long.parseLong(productsInfo[1]),
-                        Long.parseLong(productsInfo[2]),
-                        productsInfo[3]
-                );
-                inventory.add(product);
-            }
-            scannerLine++;
-        }
-        productsFile.close();
+        List<ProductDto> inventory = productService.extractProductByFile(productsFile);
         OutputView.printInventoryDetail(inventory);
+
+        //프로모션
+        Scanner promotionFile = FileReader.readFile(AddressConstant.promotionFilePath);
     }
 }
