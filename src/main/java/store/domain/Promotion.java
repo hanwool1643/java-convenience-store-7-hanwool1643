e9abf4e06a7199e44e2d657e24b6acc7ead94026
hanwool <hanwe92@gmail.com>
2024-11-08 20:33:49 +0900

    fix(Promotion): Change construct logic by using array

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,7 +1,10 @@
 package store.domain;
 
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import store.common.constants.NumberConstants;
+import store.common.constants.StringConstants;
+
 
 public class Promotion {
     private final String name;
@@ -10,13 +13,24 @@ public class Promotion {
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public Promotion(String name, Long buy, Long get, LocalDate startDate, LocalDate endDate) {
+    public Promotion(String[] promotionsInfo) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(StringConstants.DATE_FORMAT);
+
+        String name = promotionsInfo[NumberConstants.PROMOTION_NAME_INDEX];
+        long buy = Long.parseLong(promotionsInfo[NumberConstants.PROMOTION_BUY_INDEX]);
+        long get = Long.parseLong(promotionsInfo[NumberConstants.PROMOTION_GET_INDEX]);
+        LocalDate startDate = LocalDate.parse(promotionsInfo[NumberConstants.PROMOTION_START_DATE_INDEX],
+                dateFormatter);
+        LocalDate endDate = LocalDate.parse(promotionsInfo[NumberConstants.PROMOTION_END_DATE_INDEX],
+                dateFormatter);
+
         this.name = name;
         this.buy = buy;
         this.get = get;
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
     public String getName() {
         return name;
     }
@@ -29,11 +43,11 @@ public class Promotion {
         return get;
     }
 
-    public Date getStartDate() {
+    public LocalDate getStartDate() {
         return startDate;
     }
 
-    public Date getEndDate() {
+    public LocalDate getEndDate() {
         return endDate;
     }
 }
