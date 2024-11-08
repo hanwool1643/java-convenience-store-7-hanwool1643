d0db0737aec70d45d6402ea58622457747523e44
hanwool <hanwe92@gmail.com>
2024-11-08 13:14:03 +0900

    feat(StringConstants): Add product file header constants

--- src/main/java/store/common/constants/StringConstants.java
+++ src/main/java/store/common/constants/StringConstants.java
@@ -8,4 +8,6 @@ public class StringConstants {
     public static final String WON = "원";
     public static final String PIECES = "개";
     public static final String OUT_OF_STOCK = "재고 없음";
+
+    public static final String PRODUCTS_FILE_HEADER = "name,price,quantity,promotion";
 }
