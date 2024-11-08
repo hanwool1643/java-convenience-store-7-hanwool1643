f428a0096e998a400a2d9dde1ade9fabe98d4ec0
hanwool <hanwe92@gmail.com>
2024-11-08 10:35:09 +0900

    feat(StringConstants): Add StringConstants to keep string

---
+++ src/main/java/store/common/constants/StringConstants.java
@@ -0,0 +1,11 @@
+package store.common.constants;
+
+public class StringConstants {
+    public static final String COMMA = ",";
+    public static final String DASH = "-";
+    public static final String BLANK = " ";
+    
+    public static final String WON = "원";
+    public static final String PIECES = "개";
+    public static final String OUT_OF_STOCK = "재고 없음";
+}
