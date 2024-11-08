0535f1a288d8b71253f1cf22ac58d1aa1fc20f30
hanwool <hanwe92@gmail.com>
2024-11-08 20:31:08 +0900

    feat(NumberConstants): Add NumberConstants

---
+++ src/main/java/store/common/constants/NumberConstants.java
@@ -0,0 +1,17 @@
+package store.common.constants;
+
+public class NumberConstants {
+    //product row index
+    public static final int PRODUCT_NAME_INDEX = 0;
+    public static final int PRODUCT_PRICE_INDEX = 1;
+    public static final int PRODUCT_QUANTITY_INDEX = 2;
+    public static final int PRODUCT_PROMOTION_INDEX = 3;
+
+    //promotion row index
+    public static final int PROMOTION_NAME_INDEX = 0;
+    public static final int PROMOTION_BUY_INDEX = 1;
+    public static final int PROMOTION_GET_INDEX = 2;
+    public static final int PROMOTION_START_DATE_INDEX = 3;
+    public static final int PROMOTION_END_DATE_INDEX = 4;
+
+}
