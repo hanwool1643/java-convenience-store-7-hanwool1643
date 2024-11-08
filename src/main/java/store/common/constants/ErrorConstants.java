5725c890215c6fa290446f3da661b110548f3198
hanwool <hanwe92@gmail.com>
2024-11-08 10:27:49 +0900

    feat(ErrorConstants): Add ErrorConstants for keeping error message

---
+++ src/main/java/store/common/constants/ErrorConstants.java
@@ -0,0 +1,9 @@
+package store.common.constants;
+
+public class ErrorConstants {
+    public static final String PRODUCT_PURCHASE_FORMAT_ERROR_MESSAGE = "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
+    public static final String NOT_EXIST_PRODUCT_ERROR_MESSAGE = "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.";
+    public static final String INVENTORY_SHORT_ERROR_MESSAGE = "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.";
+    public static final String INPUT_FORMAT_ERROR_MESSAGE = "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.";
+    public static final String FILE_NOT_FOUND = "[ERROR] 파일을 찾을 수 없습니다.";
+}
