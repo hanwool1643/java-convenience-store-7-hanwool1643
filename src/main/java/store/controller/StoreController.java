0ca2410267dfcf134f4ee021ec1ed74560558852
hanwool <hanwe92@gmail.com>
2024-11-08 14:04:21 +0900

    fix(StoreController): Apply constants name change

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -3,7 +3,7 @@ package store.controller;
 import java.util.List;
 import java.util.Scanner;
 import store.common.FileReader;
-import store.common.constants.AddressConstant;
+import store.common.constants.AddressConstants;
 import store.domain.dto.ProductDto;
 import store.service.ProductService;
 import store.view.OutputView;
@@ -16,12 +16,12 @@ public class StoreController {
     public void open() {
         OutputView.printWelcome();
         // 제품
-        Scanner productsFile = FileReader.readFile(AddressConstant.productFilePath);
+        Scanner productsFile = FileReader.readFile(AddressConstants.productFilePath);
         ProductService productService = new ProductService();
         List<ProductDto> inventory = productService.extractProductByFile(productsFile);
         OutputView.printInventoryDetail(inventory);
 
         //프로모션
-        Scanner promotionFile = FileReader.readFile(AddressConstant.promotionFilePath);
+        Scanner promotionFile = FileReader.readFile(AddressConstants.promotionFilePath);
     }
 }
