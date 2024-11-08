031a3b070a00ed20c6dc4ca4c5424dfa4765d823
hanwool <hanwe92@gmail.com>
2024-11-08 10:41:59 +0900

    fix(StoreController): Add open method to start application

---
+++ src/main/java/store/controller/StoreController.java
@@ -0,0 +1,40 @@
+package store.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import store.common.FileReader;
+import store.common.constants.AddressConstant;
+import store.common.constants.StringConstants;
+import store.domain.dto.ProductDto;
+import store.service.ProductService;
+import store.view.InputView;
+
+public class StoreController {
+
+    public StoreController() {
+    }
+
+    public void open() {
+        Scanner productsFile = FileReader.readFile(AddressConstant.productFilePath);
+        List<ProductDto> inventory = new ArrayList<>();
+        ProductService productService = new ProductService();
+        int scannerLine = 0;
+        while (productsFile.hasNextLine()) {
+            String line = productsFile.nextLine();
+            if (scannerLine != 0) {
+                String[] productsInfo = line.split(StringConstants.COMMA);
+                ProductDto product = productService.createProduct(
+                        productsInfo[0],
+                        Long.parseLong(productsInfo[1]),
+                        Long.parseLong(productsInfo[2]),
+                        productsInfo[3]
+                );
+                inventory.add(product);
+            }
+            scannerLine++;
+        }
+        productsFile.close();
+        InputView.printInventoryDetail(inventory);
+    }
+}
