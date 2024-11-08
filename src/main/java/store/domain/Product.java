e5ba511602653164ca746e1f602c979ffb23cb00
hanwool <hanwe92@gmail.com>
2024-11-08 20:32:19 +0900

    feat(Products): Change construct logic by using array

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,12 +1,22 @@
 package store.domain;
 
+import static store.common.constants.NumberConstants.PRODUCT_NAME_INDEX;
+import static store.common.constants.NumberConstants.PRODUCT_PRICE_INDEX;
+import static store.common.constants.NumberConstants.PRODUCT_PROMOTION_INDEX;
+import static store.common.constants.NumberConstants.PRODUCT_QUANTITY_INDEX;
+
 public class Product {
     private final String name;
     private final Long price;
     private Long quantity;
     private final String promotion;
 
-    public Product(String name, Long price, Long quantity, String promotion) {
+    public Product(String[] productsInfo) {
+        String name = productsInfo[PRODUCT_NAME_INDEX];
+        long price = Long.parseLong(productsInfo[PRODUCT_PRICE_INDEX]);
+        long quantity = Long.parseLong(productsInfo[PRODUCT_QUANTITY_INDEX]);
+        String promotion = productsInfo[PRODUCT_PROMOTION_INDEX];
+
         this.name = name;
         this.price = price;
         this.quantity = quantity;
