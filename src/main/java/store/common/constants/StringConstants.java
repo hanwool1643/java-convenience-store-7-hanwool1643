48aee26edc90d8bc89601d2b98caf917c8ff1633
hanwool <hanwe92@gmail.com>
2024-11-08 20:35:22 +0900

    feat(StringConstants): Add string constants
    - date format yyyy-MM-dd
    - promotions.md header string

--- src/main/java/store/common/constants/StringConstants.java
+++ src/main/java/store/common/constants/StringConstants.java
@@ -10,4 +10,6 @@ public class StringConstants {
     public static final String OUT_OF_STOCK = "재고 없음";
 
     public static final String PRODUCTS_FILE_HEADER = "name,price,quantity,promotion";
+    public static final String PROMOTIONS_FILE_HEADER = "name,buy,get,start_date,end_date";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
 }
