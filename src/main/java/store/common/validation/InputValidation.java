6f86ec522fa907a038fabc1e9f68ea32066182c4
hanwool <hanwe92@gmail.com>
2024-11-08 21:32:06 +0900

    fix(InputValidator): Return string array after validate

--- src/main/java/store/common/validation/InputValidation.java
+++ src/main/java/store/common/validation/InputValidation.java
@@ -7,11 +7,12 @@ import store.common.constants.StringConstants;
 public class InputValidation {
     private static final String purchaseFormat = "\\[[^\\[\\]-]+\\-\\d+\\]";
 
-    public static void validatePurchaseInput(String input) {
+    public static String[] validatePurchaseInput(String input) {
         String[] splitStrings = input.split(StringConstants.COMMA);
         boolean isFormatAllMatch = Arrays.stream(splitStrings).allMatch(str -> str.trim().matches(purchaseFormat));
         if (!isFormatAllMatch) {
             throw new IllegalArgumentException(ErrorConstants.PRODUCT_PURCHASE_FORMAT_ERROR_MESSAGE);
         }
+        return splitStrings;
     }
 }
