60fd6af764aceac7dec5a5a74ffd6718bc1e1da6
hanwool <hanwe92@gmail.com>
2024-11-11 03:21:12 +0900

    feat(NumberConstants): Add few number constants

--- src/main/java/store/common/constants/NumberConstants.java
+++ src/main/java/store/common/constants/NumberConstants.java
@@ -14,4 +14,8 @@ public class NumberConstants {
     public static final int PROMOTION_START_DATE_INDEX = 3;
     public static final int PROMOTION_END_DATE_INDEX = 4;
 
+    public static final long MAX_MEMBERSHIP_DISCOUNT = 8000L;
+    public static final long NO_MEMBERSHIP_DISCOUNT = 0L;
+    public static final double MEMBERSHIP_DISCOUNT_RATIO = 0.3;
+
 }
