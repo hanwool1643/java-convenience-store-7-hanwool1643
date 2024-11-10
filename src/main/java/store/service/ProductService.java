6997c9a10a0e870470aa3c661666f2c7052173f9
hanwool <hanwe92@gmail.com>
2024-11-10 23:35:43 +0900

    feat(ProductService): Buy product by separating promotion or not

--- src/main/java/store/service/ProductService.java
+++ src/main/java/store/service/ProductService.java
@@ -1,27 +1,175 @@
 package store.service;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import store.common.constants.ErrorConstants;
 import store.common.constants.StringConstants;
 import store.domain.Product;
+import store.domain.Promotion;
+import store.domain.Receipt;
+import store.view.InputView;
 
 public class ProductService {
+    public Receipt buy(String name, Long quantity, List<Product> inventory, List<Promotion> promotions) {
+        List<Product> products = findProductByName(name, inventory);
 
-    public List<Product> extractProductByFile(Scanner productsFile) {
-        List<Product> inventory = productsFile.tokens()
-                // 첫 번째 header row 필터
-                .filter(line -> !Objects.equals(line, StringConstants.PRODUCTS_FILE_HEADER))
-                .map(line -> new Product(line.split(StringConstants.COMMA)))
-                .collect(Collectors.toList());
-        productsFile.close();
+        Product[] separatedProducts = separatePromotionProduct(products);
+        Product nonPromotionProduct = separatedProducts[0];
+        Product promotionProduct = separatedProducts[1];
 
-        return inventory;
+        if (promotionProduct != null) {
+            Promotion promotion = getApplicablePromotion(promotionProduct, promotions);
+            boolean isPromotionPeriod = promotion.calculateOnPromotion(DateTimes.now().toLocalDate());
+
+            if (isPromotionPeriod) {
+                return handlePromotionPurchase(promotionProduct, nonPromotionProduct, promotion, quantity);
+            }
+        }
+
+        return handleNonPromotionPurchase(nonPromotionProduct, quantity);
     }
 
-    public void buy(Map<String, Long> productWithQuantity, List<Product> products) {
+    // 제품 이름으로 Product 리스트를 검색
+    private List<Product> findProductByName(String name, List<Product> inventory) {
+        List<Product> products = inventory.stream()
+                .filter(component -> component.getName().equals(name))
+                .toList();
 
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException(ErrorConstants.NOT_EXIST_PRODUCT_ERROR_MESSAGE);
+        }
+        return products;
+    }
+
+    // 일반 제품과 프로모션 제품을 구분하여 배열로 반환
+    private Product[] separatePromotionProduct(List<Product> products) {
+        Supplier<Stream<Product>> productStreamSupplier = products::stream;
+
+        Product nonPromotionProduct = productStreamSupplier.get()
+                .filter(product -> StringConstants.NULL.equals(product.getPromotion()))
+                .findFirst()
+                .orElse(null);
+
+        Product promotionProduct = productStreamSupplier.get()
+                .filter(product -> !StringConstants.NULL.equals(product.getPromotion()))
+                .findFirst()
+                .orElse(null);
+
+        return new Product[]{nonPromotionProduct, promotionProduct};
+    }
+
+    // 프로모션이 적용된 제품의 Promotion 인스턴스 반환
+    private Promotion getApplicablePromotion(Product promotionProduct, List<Promotion> promotions) {
+        return promotions.stream()
+                .filter(promo -> promo.getName().equals(promotionProduct.getPromotion()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorConstants.NOT_EXIST_PROMOTION_ERROR_MESSAGE));
+    }
+
+    // 프로모션 제품 구매 처리
+    private Receipt handlePromotionPurchase(Product promotionProduct, Product nonPromotionProduct,
+                                            Promotion promotion, Long quantity) {
+        Long promotionProductQuantity = promotionProduct.getQuantity();
+        Long freeQuantity = calculateFreeQuantity(quantity, promotion);
+
+        if (promotionProductQuantity >= quantity + freeQuantity) {
+            return applyFullPromotion(promotionProduct, quantity, freeQuantity, promotion);
+        }
+        if (promotionProductQuantity < quantity + freeQuantity) {
+            return handleInsufficientPromotionStock(promotionProduct, nonPromotionProduct,
+                    promotionProductQuantity, promotion, quantity, freeQuantity);
+        }
+        throw new IllegalArgumentException(ErrorConstants.NOT_EXIST_CASE_ERROR_MESSAGE);
+    }
+
+    // 프로모션이 적용되는 상품에 대해 무료 수량 계산
+    private Long calculateFreeQuantity(Long quantity, Promotion promotion) {
+        return (quantity / promotion.getBuy()) * promotion.getGet();
+    }
+
+    // 프로모션이 전체 수량에 적용되는 경우 구매 처리
+    private Receipt applyFullPromotion(Product promotionProduct, Long quantity, Long freeQuantity,
+                                       Promotion promotion) {
+        if (quantity % promotion.getBuy() == 0) {
+            String answer = InputView.tellFreeProductProvide(promotionProduct.getName(), freeQuantity);
+            Receipt productReceipt = buyProductWithPromotionOrNot(promotionProduct, quantity, freeQuantity, answer);
+            if (productReceipt != null) {
+                return productReceipt;
+            }
+        }
+        promotionProduct.buy(quantity + freeQuantity);
+        return new Receipt(promotionProduct.getName(), quantity + freeQuantity, freeQuantity,
+                promotionProduct.getPrice());
+    }
+
+    // 프로모션 할인 구매 적용에 따른 구매 처리
+    private static Receipt buyProductWithPromotionOrNot(Product promotionProduct, Long quantity, Long freeQuantity, String answer) {
+        if (StringConstants.YES.equals(answer)) {
+            promotionProduct.buy(quantity + freeQuantity);
+            return new Receipt(promotionProduct.getName(), quantity + freeQuantity, freeQuantity,
+                    promotionProduct.getPrice());
+        }
+        if (StringConstants.NO.equals(answer)) {
+            promotionProduct.buy(quantity);
+            return new Receipt(promotionProduct.getName(), quantity, 0L, promotionProduct.getPrice());
+        }
+        return null;
+    }
+
+    // 프로모션 재고가 부족한 경우의 처리
+    private Receipt handleInsufficientPromotionStock(Product promotionProduct, Product nonPromotionProduct, Long promotionProductQuantity,
+                                                     Promotion promotion, Long quantity, Long freeQuantity) {
+        Long promotionNotAppliedQuantity = promotionProductQuantity % (promotion.getBuy() + promotion.getGet());
+        String answer = InputView.tellPromotionNotApplicable(promotionProduct.getName(), promotionNotAppliedQuantity);
+
+        return buyProductByAnswer(promotionProduct, nonPromotionProduct, promotionProductQuantity, quantity,
+                freeQuantity,
+                answer, promotionNotAppliedQuantity);
+    }
+
+    // 프로모션 재고 부족 시 부족분 정가 구매 혹은 미구매 처리
+    private static Receipt buyProductByAnswer(Product promotionProduct, Product nonPromotionProduct, Long promotionProductQuantity,
+                                              Long quantity, Long freeQuantity, String answer, Long promotionNotAppliedQuantity) {
+        Receipt allProductReceipt = answerYes(promotionProduct, nonPromotionProduct, promotionProductQuantity,
+                quantity, freeQuantity, answer, promotionNotAppliedQuantity);
+        if (allProductReceipt != null) return allProductReceipt;
+
+        Receipt promotionProductReceipt = answerNo(promotionProduct, quantity, freeQuantity, answer,
+                promotionNotAppliedQuantity);
+        if (promotionProductReceipt != null) return promotionProductReceipt;
+
+        throw new IllegalArgumentException(ErrorConstants.INPUT_FORMAT_ERROR_MESSAGE);
+    }
+
+    // 프로모션 재고 부족 시 부족분 미구매
+    private static Receipt answerNo(Product promotionProduct, Long quantity, Long freeQuantity,
+                                    String answer, Long promotionNotAppliedQuantity) {
+        if (StringConstants.NO.equals(answer)) {
+            long quantityToBuy = quantity - promotionNotAppliedQuantity;
+            promotionProduct.buy(quantityToBuy);
+            return new Receipt(promotionProduct.getName(), quantityToBuy, freeQuantity - promotionNotAppliedQuantity,
+                    promotionProduct.getPrice());
+        }
+        return null;
+    }
+
+    // 프로모션 재고 부족 시 부족분 정가 구매
+    private static Receipt answerYes(Product promotionProduct, Product nonPromotionProduct, Long promotionProductQuantity,
+                                     Long quantity, Long freeQuantity, String answer, Long promotionNotAppliedQuantity) {
+        if (StringConstants.YES.equals(answer)) {
+            promotionProduct.buy(promotionProductQuantity);
+            nonPromotionProduct.buy(quantity - promotionProductQuantity);
+            return new Receipt(promotionProduct.getName(), quantity, freeQuantity - promotionNotAppliedQuantity,
+                    promotionProduct.getPrice());
+        }
+        return null;
+    }
+
+    // 프로모션이 적용되지 않는 일반 상품 구매 처리
+    private Receipt handleNonPromotionPurchase(Product nonPromotionProduct, Long quantity) {
+        nonPromotionProduct.buy(quantity);
+        return new Receipt(nonPromotionProduct.getName(), quantity, 0L, nonPromotionProduct.getPrice());
     }
 }
