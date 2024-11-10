874080a223da22193a10dcf7eea72149970e7d89
hanwool <hanwe92@gmail.com>
2024-11-10 22:35:56 +0900

    fix(PromotionService): Move extract file method to fileService

--- src/main/java/store/service/PromotionService.java
+++ src/main/java/store/service/PromotionService.java
@@ -1,22 +1,5 @@
 package store.service;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import store.common.constants.StringConstants;
-import store.domain.Promotion;
-
 public class PromotionService {
 
-    public List<Promotion> extractPromotionByFile(Scanner promotionsFile) {
-        List<Promotion> promotions = promotionsFile.tokens()
-                // 첫 번째 header row 필터
-                .filter(line -> !Objects.equals(line, StringConstants.PROMOTIONS_FILE_HEADER))
-                .map(line -> new Promotion(line.split(StringConstants.COMMA)))
-                .collect(Collectors.toList());
-        promotionsFile.close();
-
-        return promotions;
-    }
 }
