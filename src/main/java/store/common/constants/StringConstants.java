9a02026701d0e3bbeadd84d4ece998d29d2952d0
hanwool <hanwe92@gmail.com>
2024-11-11 08:59:20 +0900

    feat(StringConstants): Add receipt print constants

--- src/main/java/store/common/constants/StringConstants.java
+++ src/main/java/store/common/constants/StringConstants.java
@@ -15,4 +15,17 @@ public class StringConstants {
     public static final String NO = "N";
     public static final String YES = "Y";
     public static final String NULL = "null";
+
+    public static final String RECEIPT_HEADER = "===============W 편의점===============";
+    public static final String PROMOTION_SECTION_HEADER = "===============증  정================";
+    public static final String SUMMARY_SEPARATOR = "====================================";
+    public static final String RECEIPT_PRODUCT_NAME = "상품명";
+    public static final String RECEIPT_PRODUCT_QUANTITY = "수량";
+    public static final String RECEIPT_PRODUCT_PRICE = "금액";
+    public static final String PROMOTION_DISCOUNT = "행사할인";
+    public static final String MEMBERSHIP_DISCOUNT = "멤버십할인";
+    public static final String TOTAL_PRICE_AMOUNT = "총구매액";
+    public static final String MONEY_TO_PAY = "내실돈";
+
+
 }
