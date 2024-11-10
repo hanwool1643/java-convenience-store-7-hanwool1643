8ad53481148141e85d8ef2d1c897967334b20c26
hanwool <hanwe92@gmail.com>
2024-11-10 23:05:54 +0900

    fix(InputView): Change some method
    - change return value to map
    - make method to get yes or no

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,40 +1,60 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
+import store.common.constants.ErrorConstants;
 import store.common.constants.MessageConstants;
+import store.common.constants.StringConstants;
 import store.common.validation.InputValidation;
 
 public class InputView {
 
-    public static String[] askPurchaseProduct() {
+    public static Map<String, Long> askPurchaseProduct() {
         System.out.println(MessageConstants.PRODUCT_PURCHASE_MESSAGE);
         while (true) {
             try {
                 String input = Console.readLine();
-                return InputValidation.validatePurchaseInput(input);
+                String[] inputSplit = InputValidation.validatePurchaseInput(input);
+                return InputParser.purchaseInputParser(inputSplit);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public void tellPromotionNotApplicable() {
-        System.out.println(MessageConstants.PROMOTION_NOT_APPLY_MESSAGE);
-        Console.readLine();
+    public static String tellPromotionNotApplicable(String name, Long quantity) {
+        System.out.printf(MessageConstants.PROMOTION_NOT_APPLY_MESSAGE, name, quantity);
+        System.out.println();
+        return getYesOrNoAnswer();
     }
 
-    public void tellFreeProductProvide() {
-        System.out.println(MessageConstants.PROMOTION_PROVIDE_FREE_PRODUCT_MESSAGE);
-        Console.readLine();
+    private static String getYesOrNoAnswer() {
+        while(true) {
+            try {
+                String answer = Console.readLine();
+                if (!(answer.equals(StringConstants.NO) || answer.equals(StringConstants.YES))) {
+                    throw new IllegalArgumentException(ErrorConstants.INPUT_FORMAT_ERROR_MESSAGE);
+                }
+                return answer;
+            } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public void askMembershipDiscount() {
+    public static String tellFreeProductProvide(String name, Long quantity) {
+        System.out.printf(MessageConstants.PROMOTION_PROVIDE_FREE_PRODUCT_MESSAGE, name, quantity);
+        System.out.println();
+        return getYesOrNoAnswer();
+    }
+
+    public static String askMembershipDiscount() {
         System.out.println(MessageConstants.MEMBERSHIP_DISCOUNT_MESSAGE);
-        Console.readLine();
+        return Console.readLine();
     }
 
-    public void buyAnotherProduct() {
+    public static String buyAnotherProduct() {
         System.out.println(MessageConstants.EXIT_MESSAGE);
-        Console.readLine();
+        return Console.readLine();
     }
 }
