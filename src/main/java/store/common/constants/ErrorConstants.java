dcb50fef21f531bba02f828bfd1bd5752abd1281
hanwool <hanwe92@gmail.com>
2024-11-10 21:26:56 +0900

    feat(ErrorConstants): Add promotion not found error

--- src/main/java/store/common/constants/ErrorConstants.java
+++ src/main/java/store/common/constants/ErrorConstants.java
@@ -6,4 +6,5 @@ public class ErrorConstants {
     public static final String INVENTORY_SHORT_ERROR_MESSAGE = "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.";
     public static final String INPUT_FORMAT_ERROR_MESSAGE = "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.";
     public static final String FILE_NOT_FOUND = "[ERROR] 파일을 찾을 수 없습니다.";
+    public static final String NOT_EXIST_PROMOTION_ERROR_MESSAGE = "[ERROR] 프로모션을 찾을 수 없습니다.";
 }
