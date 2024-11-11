6b64fdab39886ce8265094677f86de1f6ea7678b
hanwool <hanwe92@gmail.com>
2024-11-11 10:27:38 +0900

    feat(Application): Inject impl to controller not to construct in itself

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,10 +1,17 @@
 package store;
 
 import store.controller.StoreController;
+import store.service.FileService;
+import store.service.FileServiceImpl;
+import store.service.StoreService;
+import store.service.StoreServiceImpl;
 
 public class Application {
     public static void main(String[] args) {
-        StoreController storeController = new StoreController();
+        FileService fileService = new FileServiceImpl();
+        StoreService productService = new StoreServiceImpl();
+
+        StoreController storeController = new StoreController(fileService, productService);
         storeController.run();
     }
 }
