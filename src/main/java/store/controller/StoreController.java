fc7dcc19b91642b124edf49168553324a6be505c
hanwool <hanwe92@gmail.com>
2024-11-11 10:14:24 +0900

    fix(service): Change service to impl to use interface

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -10,8 +10,8 @@ import store.common.constants.StringConstants;
 import store.domain.Product;
 import store.domain.Promotion;
 import store.domain.Receipt;
-import store.service.FileService;
-import store.service.StoreService;
+import store.service.FileServiceImpl;
+import store.service.StoreServiceImpl;
 import store.view.InputView;
 import store.view.OutputView;
 
@@ -20,56 +20,56 @@ public class StoreController {
     public StoreController() {
     }
 
-    public void open() {
-        FileService fileService = new FileService();
-        StoreService storeService = new StoreService();
+    public void run() {
+        FileServiceImpl fileServiceImpl = new FileServiceImpl();
+        StoreServiceImpl storeServiceImpl = new StoreServiceImpl();
 
-        List<Product> inventory = extractProducts(fileService); // 제품 추출
-        List<Promotion> promotions = extractPromotions(fileService); //프로모션 추출
+        List<Product> inventory = extractProducts(fileServiceImpl); // 제품 추출
+        List<Promotion> promotions = extractPromotions(fileServiceImpl); //프로모션 추출
 
         while (true) {
-            String answerToContinue = processPurchase(inventory, storeService, promotions);
+            String answerToContinue = processPurchase(inventory, storeServiceImpl, promotions);
             if (answerToContinue.equals(StringConstants.NO)) break;
         }
     }
 
-    private static List<Promotion> extractPromotions(FileService fileService) {
+    private static List<Promotion> extractPromotions(FileServiceImpl fileServiceImpl) {
         Scanner promotionFile = FileReader.readFile(AddressConstants.promotionFilePath);
-        return fileService.extractPromotionByFile(promotionFile);
+        return fileServiceImpl.extractPromotionByFile(promotionFile);
     }
 
-    private static List<Product> extractProducts(FileService fileService) {
+    private static List<Product> extractProducts(FileServiceImpl fileServiceImpl) {
         Scanner productsFile = FileReader.readFile(AddressConstants.productFilePath);
-        return fileService.extractProductByFile(productsFile);
+        return fileServiceImpl.extractProductByFile(productsFile);
     }
 
-    private static String processPurchase(List<Product> inventory, StoreService storeService, List<Promotion> promotions) {
+    private static String processPurchase(List<Product> inventory, StoreServiceImpl storeServiceImpl, List<Promotion> promotions) {
         OutputView.printWelcome(); // 환영 인사
         OutputView.printInventoryDetail(inventory); // 재고 확인
 
-        List<Receipt> receipts = buyProducts(storeService, inventory, promotions); // 구매
-        Long[] totalReceipts = storeService.calculateTotalReceipts(receipts); // totalReceipts[0]: 총구매 금액, totalReceipts[1]: 총할인 금액
+        List<Receipt> receipts = buyProducts(storeServiceImpl, inventory, promotions); // 구매
+        Long[] totalReceipts = storeServiceImpl.calculateTotalReceipts(receipts); // totalReceipts[0]: 총구매 금액, totalReceipts[1]: 총할인 금액
         Long priceAfterPromotionDiscount = totalReceipts[0] - totalReceipts[1];
-        Long membershipDiscount = calculateMembershipDiscount(storeService, priceAfterPromotionDiscount); // 멤버십 할인 적용
+        Long membershipDiscount = calculateMembershipDiscount(storeServiceImpl, priceAfterPromotionDiscount); // 멤버십 할인 적용
 
         OutputView.printFinalReceipt(receipts, totalReceipts[1], membershipDiscount); // 영수증 출력
 
         return InputView.buyAnotherProduct(); // 물건 반복 구매 문의
     }
 
-    private static Long calculateMembershipDiscount(StoreService storeService, Long priceAfterPromotionDiscount) {
+    private static Long calculateMembershipDiscount(StoreServiceImpl storeServiceImpl, Long priceAfterPromotionDiscount) {
         String answerToApplyMembership = InputView.askMembershipDiscount();
-        return storeService.applyMembershipOrNot(priceAfterPromotionDiscount, answerToApplyMembership);
+        return storeServiceImpl.applyMembershipOrNot(priceAfterPromotionDiscount, answerToApplyMembership);
     }
 
-    private static List<Receipt> buyProducts(StoreService storeService, List<Product> inventory,
+    private static List<Receipt> buyProducts(StoreServiceImpl storeServiceImpl, List<Product> inventory,
                                              List<Promotion> promotions) {
         List<Receipt> receipts = new ArrayList<>();
         while (receipts.isEmpty()) {
             try {
                 Map<String, Long> productsToBuy = InputView.askPurchaseProduct();
                 productsToBuy.forEach((name, quantity) -> {
-                    Receipt receipt = storeService.buy(name, quantity, inventory, promotions);
+                    Receipt receipt = storeServiceImpl.buy(name, quantity, inventory, promotions);
                     receipts.add(receipt);
                 });
             } catch (IllegalArgumentException e) {
---
+++ src/main/java/store/service/FileServiceImpl.java
@@ -0,0 +1,34 @@
+package store.service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import store.common.constants.StringConstants;
+import store.domain.Product;
+import store.domain.Promotion;
+
+public class FileServiceImpl {
+
+    public List<Product> extractProductByFile(Scanner productsFile) {
+        List<Product> inventory = productsFile.tokens()
+                // 첫 번째 header row 필터
+                .filter(line -> !Objects.equals(line, StringConstants.PRODUCTS_FILE_HEADER))
+                .map(line -> new Product(line.split(StringConstants.COMMA)))
+                .collect(Collectors.toList());
+        productsFile.close();
+
+        return inventory;
+    }
+
+    public List<Promotion> extractPromotionByFile(Scanner promotionsFile) {
+        List<Promotion> promotions = promotionsFile.tokens()
+                // 첫 번째 header row 필터
+                .filter(line -> !Objects.equals(line, StringConstants.PROMOTIONS_FILE_HEADER))
+                .map(line -> new Promotion(line.split(StringConstants.COMMA)))
+                .collect(Collectors.toList());
+        promotionsFile.close();
+
+        return promotions;
+    }
+}
