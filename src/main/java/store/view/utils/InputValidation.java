273c66a0fbbd3a16ccf7369a269b0ccfeb276a07
hanwool <hanwe92@gmail.com>
2024-11-11 10:30:09 +0900

    fix(InputValidation): Apply purchase constants

--- src/main/java/store/view/utils/InputValidation.java
+++ src/main/java/store/view/utils/InputValidation.java
@@ -5,11 +5,9 @@ import store.common.constants.ErrorConstants;
 import store.common.constants.StringConstants;
 
 public class InputValidation {
-    private static final String purchaseFormat = "\\[[^\\[\\]-]+\\-\\d+\\]";
-
     public static String[] validatePurchaseInput(String input) {
         String[] inputSplit = input.split(StringConstants.COMMA);
-        boolean isFormatAllMatch = Arrays.stream(inputSplit).allMatch(str -> str.trim().matches(purchaseFormat));
+        boolean isFormatAllMatch = Arrays.stream(inputSplit).allMatch(str -> str.trim().matches(StringConstants.PURCHASE_INPUT_FORMAT));
         if (!isFormatAllMatch) {
             throw new IllegalArgumentException(ErrorConstants.PRODUCT_PURCHASE_FORMAT_ERROR_MESSAGE);
         }
