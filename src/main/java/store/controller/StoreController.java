16a704f36e9a5a6507f25c233d29057b07929e33
hanwool <hanwe92@gmail.com>
2024-11-11 10:26:53 +0900

    feat(StoreController): Apply dependency injection to methods

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -10,66 +10,69 @@ import store.common.constants.StringConstants;
 import store.domain.Product;
 import store.domain.Promotion;
 import store.domain.Receipt;
+import store.service.FileService;
 import store.service.FileServiceImpl;
+import store.service.StoreService;
 import store.service.StoreServiceImpl;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class StoreController {
+    private final FileService fileService;
+    private final StoreService storeService;
 
-    public StoreController() {
+    public StoreController(FileService fileService, StoreService storeService) {
+        this.fileService = fileService;
+        this.storeService = storeService;
     }
 
     public void run() {
-        FileServiceImpl fileServiceImpl = new FileServiceImpl();
-        StoreServiceImpl storeServiceImpl = new StoreServiceImpl();
-
-        List<Product> inventory = extractProducts(fileServiceImpl); // 제품 추출
-        List<Promotion> promotions = extractPromotions(fileServiceImpl); //프로모션 추출
+        List<Product> inventory = extractProducts(fileService); // 제품 추출
+        List<Promotion> promotions = extractPromotions(fileService); //프로모션 추출
 
         while (true) {
-            String answerToContinue = processPurchase(inventory, storeServiceImpl, promotions);
+            String answerToContinue = processPurchase(inventory, storeService, promotions);
             if (answerToContinue.equals(StringConstants.NO)) break;
         }
     }
 
-    private static List<Promotion> extractPromotions(FileServiceImpl fileServiceImpl) {
+    private static List<Promotion> extractPromotions(FileService fileServiceImpl) {
         Scanner promotionFile = FileReader.readFile(AddressConstants.promotionFilePath);
         return fileServiceImpl.extractPromotionByFile(promotionFile);
     }
 
-    private static List<Product> extractProducts(FileServiceImpl fileServiceImpl) {
+    private static List<Product> extractProducts(FileService fileServiceImpl) {
         Scanner productsFile = FileReader.readFile(AddressConstants.productFilePath);
         return fileServiceImpl.extractProductByFile(productsFile);
     }
 
-    private static String processPurchase(List<Product> inventory, StoreServiceImpl storeServiceImpl, List<Promotion> promotions) {
+    private static String processPurchase(List<Product> inventory, StoreService storeService, List<Promotion> promotions) {
         OutputView.printWelcome(); // 환영 인사
         OutputView.printInventoryDetail(inventory); // 재고 확인
 
-        List<Receipt> receipts = buyProducts(storeServiceImpl, inventory, promotions); // 구매
-        Long[] totalReceipts = storeServiceImpl.calculateTotalReceipts(receipts); // totalReceipts[0]: 총구매 금액, totalReceipts[1]: 총할인 금액
+        List<Receipt> receipts = buyProducts(storeService, inventory, promotions); // 구매
+        Long[] totalReceipts = storeService.calculateTotalReceipts(receipts); // totalReceipts[0]: 총구매 금액, totalReceipts[1]: 총할인 금액
         Long priceAfterPromotionDiscount = totalReceipts[0] - totalReceipts[1];
-        Long membershipDiscount = calculateMembershipDiscount(storeServiceImpl, priceAfterPromotionDiscount); // 멤버십 할인 적용
+        Long membershipDiscount = calculateMembershipDiscount(storeService, priceAfterPromotionDiscount); // 멤버십 할인 적용
 
         OutputView.printFinalReceipt(receipts, totalReceipts[1], membershipDiscount); // 영수증 출력
 
         return InputView.buyAnotherProduct(); // 물건 반복 구매 문의
     }
 
-    private static Long calculateMembershipDiscount(StoreServiceImpl storeServiceImpl, Long priceAfterPromotionDiscount) {
+    private static Long calculateMembershipDiscount(StoreService storeService, Long priceAfterPromotionDiscount) {
         String answerToApplyMembership = InputView.askMembershipDiscount();
-        return storeServiceImpl.applyMembershipOrNot(priceAfterPromotionDiscount, answerToApplyMembership);
+        return storeService.applyMembershipOrNot(priceAfterPromotionDiscount, answerToApplyMembership);
     }
 
-    private static List<Receipt> buyProducts(StoreServiceImpl storeServiceImpl, List<Product> inventory,
+    private static List<Receipt> buyProducts(StoreService storeService, List<Product> inventory,
                                              List<Promotion> promotions) {
         List<Receipt> receipts = new ArrayList<>();
         while (receipts.isEmpty()) {
             try {
                 Map<String, Long> productsToBuy = InputView.askPurchaseProduct();
                 productsToBuy.forEach((name, quantity) -> {
-                    Receipt receipt = storeServiceImpl.buy(name, quantity, inventory, promotions);
+                    Receipt receipt = storeService.buy(name, quantity, inventory, promotions);
                     receipts.add(receipt);
                 });
             } catch (IllegalArgumentException e) {
